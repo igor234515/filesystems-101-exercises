748603a228501b10d928014cd05cce578d910af4
Igor <zhukov.ia@phystech.edu>
2023-12-10 23:59:15 +0300

    12-grpc-prometheus v1

--- 12-grpc-prometheus/pkg/parhash/server.go
+++ 12-grpc-prometheus/pkg/parhash/server.go
@@ -2,10 +2,19 @@ package parhash
 
 import (
 	"context"
+	"log"
+	"net"
+	"sync"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/sync/semaphore"
+	"google.golang.org/grpc"
+
+	hashpb "fs101ex/pkg/gen/hashsvc"
+	parhashpb "fs101ex/pkg/gen/parhashsvc"
+	"fs101ex/pkg/workgroup"
 )
 
 type Config struct {
@@ -52,31 +61,154 @@ type Config struct {
 //
 // Both performance counters must be placed to Prometheus namespace "parhash".
 type Server struct {
-	conf Config
+	config Config
 
-	sem *semaphore.Weighted
+	listener   net.Listener
+	work_group sync.WaitGroup
+	cancel     context.CancelFunc
+
+	profiler  *Profiler
+	semaphore *semaphore.Weighted
+
+	mutex      sync.Mutex
+	backend_ID int
+}
+
+type Profiler struct {
+	nr_nr_requests     prometheus.Counter
+	subquery_durations *prometheus.HistogramVec
+}
+
+func NewProfiler(registerer prometheus.Registerer) *Profiler {
+
+	profiler := &Profiler{
+		nr_nr_requests: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: "parhash",
+			Name:      "nr_requests",
+			Help:      "Call ParalellHash counter",
+		}),
+		subquery_durations: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: "parhash",
+			Name:      "subquery_durations",
+			Buckets:   prometheus.ExponentialBuckets(0.1, 1e4, 24),
+			Help:      "Duration Histogram",
+		},
+
+			[]string{"backend"}),
+	}
+
+	registerer.MustRegister(profiler.subquery_durations)
+	registerer.MustRegister(profiler.nr_nr_requests)
+	return profiler
 }
 
 func New(conf Config) *Server {
 	return &Server{
-		conf: conf,
-		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
+		semaphore: semaphore.NewWeighted(int64(conf.Concurrency)),
+		config:    conf,
 	}
 }
 
-func (s *Server) Start(ctx context.Context) (err error) {
-	defer func() { err = errors.Wrap(err, "Start()") }()
+func (s *Server) Start(ctx context.Context) (status error) {
+	defer func() { status = errors.Wrap(status, "Start()") }()
 
 	/* implement me */
+
+	ctx, s.cancel = context.WithCancel(ctx)
+
+	s.listener, status = net.Listen("tcp", s.config.ListenAddr)
+
+	if status != nil {
+		return status
+	}
+
+	server := grpc.NewServer()
+	parhashpb.RegisterParallelHashSvcServer(server, s)
+
+	s.work_group.Add(2)
+
+	go func() {
+		defer s.work_group.Done()
+
+		server.Serve(s.listener)
+	}()
+
+	go func() {
+		defer s.work_group.Done()
+
+		<-ctx.Done()
+		s.listener.Close()
+	}()
 
 	return nil
 }
 
 func (s *Server) ListenAddr() string {
 	/* implement me */
-	return ""
+	return s.listener.Addr().String()
 }
 
 func (s *Server) Stop() {
 	/* implement me */
+	s.cancel()
+	s.work_group.Wait()
+}
+
+func (s *Server) ParallelHash(ctx context.Context, req *parhashpb.ParHashReq) (responce *parhashpb.ParHashResp, status error) {
+	s.profiler.nr_nr_requests.Inc()
+	var (
+		backends_number = len(s.config.BackendAddrs)
+		connections     = make([]*grpc.ClientConn, backends_number)
+		clients         = make([]hashpb.HashSvcClient, backends_number)
+		work_group      = workgroup.New(workgroup.Config{Sem: s.semaphore})
+		hashes          = make([][]byte, len(req.Data))
+	)
+
+	for i := range clients {
+
+		connections[i], status = grpc.Dial(s.config.BackendAddrs[i],
+			grpc.WithInsecure())
+
+		if status != nil {
+			log.Fatalf("failed to connect to %q: %v", s.config.BackendAddrs[i], status)
+		}
+
+		defer connections[i].Close()
+
+		clients[i] = hashpb.NewHashSvcClient(connections[i])
+	}
+
+	for i := range req.Data {
+		hash_ind := i
+
+		work_group.Go(ctx, func(ctx context.Context) (status error) {
+
+			s.mutex.Lock()
+			backend_ind := s.backend_ID
+			s.backend_ID = (s.backend_ID + 1) % backends_number
+			s.mutex.Unlock()
+
+			start_time := time.Now()
+			responce, status := clients[backend_ind].Hash(ctx, &hashpb.HashReq{Data: req.Data[hash_ind]})
+			end_time := time.Since(start_time)
+
+			s.profiler.subquery_durations.With(prometheus.Labels{"backend": s.config.BackendAddrs[backend_ind]}).Observe(float64(end_time.Microseconds()))
+
+			if status != nil {
+				return status
+			}
+
+			s.mutex.Lock()
+			hashes[hash_ind] = responce.Hash
+			s.mutex.Unlock()
+
+			return nil
+		})
+	}
+
+	if status := work_group.Wait(); status != nil {
+		log.Fatalf("failed to hash files: %v", status)
+	}
+
+	return &parhashpb.ParHashResp{Hashes: hashes}, nil
 }
